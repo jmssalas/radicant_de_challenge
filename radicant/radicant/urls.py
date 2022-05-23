9a8fc599c1e07f9b945587423fb63e3c7340c461
jmssalas <josemaria.sanchezsalas@gmail.com>
2022-05-23 14:50:15 +0200

    Add swagger-ui route

--- radicant/radicant/urls.py
+++ radicant/radicant/urls.py
@@ -4,11 +4,11 @@ from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('api/v1/', include('api.urls')),
-    path('docs-schema/', get_schema_view(
+    path('swagger-ui-schema/', get_schema_view(
         title="radicant Challenge"
-    ), name='docs-schema'),
-    path('docs/', TemplateView.as_view(
+    ), name='swagger-ui-schema'),
+    path('swagger-ui/', TemplateView.as_view(
         template_name='documentation.html',
-        extra_context={'schema_url':'docs-schema'}
+        extra_context={'schema_url':'swagger-ui-schema'}
     ), name='swagger-ui'),
 ]
